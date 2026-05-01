304930c97f048e784db3b955610d0cc40eee81df
svenwegener <sven@sven-wegener.de>
2026-05-01 17:43:39 +0200

    Initial Commit

---
+++ DietCodeiOS/DietCodeiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  DietCodeiOSApp.swift
+//  DietCodeiOS
+//
+//  Created by Sven Wegener on 01.05.26.
+//
+
+import SwiftUI
+
+@main
+struct DietCodeiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
